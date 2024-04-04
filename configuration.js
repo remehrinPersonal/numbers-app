bacc59eaa62c6b7a2d3c6f51e2e2ebee7595b611
Ross Mehringer <rossmehringer42@gmail.com>
2024-04-04 17:28:48 -0400

    Pr12

--- configuration.js
+++ configuration.js
@@ -1,14 +1,14 @@
 const hosts_local = {
-    arithmetic_servoce: "http://localhost:4000",
+    arithmetic_service: "http://localhost:4000",
     primes_service: "http://localhost:8085",
     ciphers_service: "http://localhost:4006",
     quotation_service: "http://localhost:4004"
 };
 
 const hosts_remote = {
-   arithmetic_servoce: "https://arithmetic-service.onrender.com",
-   primes_service: "https://primes-service.onrender.com",
-   ciphers_service: "https://ciphers-service-hbahramian.onrender.com",
+   arithmetic_service: "https://arithmetic-service-remehrinpersonal.onrender.com",
+   primes_service: "https://primes-service-latest-mn3n.onrender.com",
+   ciphers_service: "https://ciphers-service-remehrinpersonal.onrender.com",
    quotation_service: "?"
 };
 
